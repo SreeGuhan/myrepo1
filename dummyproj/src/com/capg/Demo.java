758f06d73fe86549235a7c326314940d78f3e6bc
learning <learning@DIN66007766.corp.capgemini.com>
2018-07-06 17:32:59 +0530

    cvsvsvs

---
+++ dummyproj/src/com/capg/Demo.java
@@ -0,0 +1,12 @@
+package com.capg;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi");
+		System.out.println("Welcome");
+
+	}
+
+}
